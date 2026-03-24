8303e96004618c867dd63fb8428d658f23b74a18
Trevor <trevor.abercrombie@gmail.com>
2026-03-24 02:08:09 -0600

    install immy and restart

--- setup.sh
+++ setup.sh
@@ -23,14 +23,17 @@ ln -s $root/config/qt6ct         ~/.config
 ln -s $root/config/xsettingsd    ~/.config
 
 
-ln -s $root/gtkrc-2.0            ~/.gtkrc-2.0
-
 ln -s $root/bashrc               ~/.bashrc
 ln -s $root/bash_aliases         ~/.bash_aliases
 ln -s $root/bash_profile         ~/.bash_profile
+ln -s $root/gtkrc-2.0            ~/.gtkrc-2.0
+
+fc-cache -fv
 
 sudo cp $root/systemd/getty@tty1.service /etc/systemd/system/getty@tty1.service
 sudo sed -i s/USER/$USER/ /etc/systemd/system/getty@tty1.service
+
+cd ~/dev
 
 git clone https://aur.archlinux.org/yay.git
 cd yay
@@ -42,14 +45,25 @@ yay -Syu --noconfirm - < packages.txt
 
 systemctl --user enable pipewire pipewire-pulse wireplumber swaync gnome-keyring-daemon
 
-fc-cache -fv
-
 # configure zen
-ZEN_PROFILE_PATH=$(find ~/.config/zen -maxdepth 1 -type d -name "*.Default (release)" | head -n 1)
-ZEN_USER_JS="$ZEN_PROFILE_PATH/user.js"
+ZEN_USER_JS="$(find ~/.config/zen -maxdepth 1 -type d -name "*.Default (release)" | head -n 1)/user.js"
 ZEN_SETTING1='user_pref("zen.view.experimental-no-window-controls", true);'
 ZEN_SETTING2='user_pref("zen.theme.content-element-separation", 0);'
 grep -qF "$ZEN_SETTING1" "$ZEN_USER_JS" 2>/dev/null || echo "$ZEN_SETTING1" >> "$ZEN_USER_JS"
 grep -qF "$ZEN_SETTING2" "$ZEN_USER_JS" 2>/dev/null || echo "$ZEN_SETTING2" >> "$ZEN_USER_JS"
+
+cd ~/dev
+git clone https://github.com/ztchary/immy
+cd immy
+make immy
+sudo make install
+grep -Po '(?<=^MimeType=).*' ~/.local/share/applications/immy.desktop | tr ';' '\n' | sed '/^$/d' | xargs -I {} xdg-mime default immy.desktop {}
+
+cd ~/dev
+git clone https://github.com/ztchary/restart
+cd restart
+make restart
+sudo make install
+
 
 fastfetch
