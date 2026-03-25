a6a8fa8a840862207b3d51083305027aced10333
Trevor Abercrombie <trevor.abercrombie@gmail.com>
2026-03-24 23:52:41 -0600

    update repo on get

--- get.sh
+++ get.sh
@@ -1,6 +1,13 @@
+#!/usr/bin/env bash
+set -e
+
 mkdir -p ~/dev
 cd ~/dev
+
 sudo pacman -Syu --needed --noconfirm git
-git clone https://github.com/tsevor/dotfiles
+
+[ ! -d ~/dev/dotfiles ] && git clone https://github.com/tsevor/dotfiles
 cd ~/dev/dotfiles
+git pull
+
 bash ~/dev/dotfiles/setup.sh
