f54d8d540019aa63cb5e56aa549c2fe9f77ff03c
Trevor Abercrombie <trevor.abercrombie@gmail.com>
2026-03-24 18:27:48 -0600

    fix my buffoonery

--- get.sh
+++ get.sh
@@ -1,6 +1,6 @@
 mkdir -p ~/dev
 cd ~/dev
-sudo pacman -Sy --needed git
+sudo pacman -Sy --needed --noconfirm git
 git clone https://github.com/tsevor/dotfiles
 cd ~/dev/dotfiles
 bash ~/dev/dotfiles/setup.sh
