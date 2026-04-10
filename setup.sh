c5b69d29a8f95439d05e4a787f2fe7972fa1cf56
Trevor Abercrombie <trevor.abercrombie@gmail.com>
2026-04-10 14:22:24 -0600

    fix machine.conf generation

--- setup.sh
+++ setup.sh
@@ -38,6 +38,7 @@ rm -f ~/.bash_profile       ; ln -s "$root/home/bash_profile"      ~/.bash_profi
 rm -f ~/.gtkrc-2.0          ; ln -s "$root/home/gtkrc-2.0"         ~/.gtkrc-2.0
 
 # create file for untracked machine specific config in hyprland
+[ ! -f ~/.config/hypr/land/machine.conf ]  && \
 tee ~/.config/hypr/land/machine.conf > /dev/null << EOF
 # this file is for machine specific config untracked by git
 # or to make your own small changes without git yelling at you
